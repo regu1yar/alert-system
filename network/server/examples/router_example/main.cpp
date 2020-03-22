f4fe1954b227ddde2a058637a473e07cbe5004c2
Roman <roma_klimovitskiy@mail.ru>
2020-03-23 01:19:09 +0300

    Made client example more clear

--- network/server/examples/router_example/main.cpp
+++ network/server/examples/router_example/main.cpp
@@ -1,40 +1,14 @@
 #include <iostream>
-#include <condition_variable>
-#include <mutex>
-#include <signal.h>
 
 #include "handler.h"
 #include "router_controller.h"
+#include "runtime_utils.h"
 
 class HelloWorldHandler : public network::Handler {
  public:
   void handleGet(network::http_request message) override {
     std::cout << "Responded to world!" << std::endl;
     message.reply(web::http::status_codes::Accepted, "Hello, world!");
-  }
-};
-
-static std::condition_variable _condition;
-static std::mutex _mutex;
-
-class InterruptHandler {
- public:
-  static void hookSIGINT() {
-    signal(SIGINT, handleUserInterrupt);
-  }
-
-  static void handleUserInterrupt(int signal){
-    if (signal == SIGINT) {
-      std::cout << "SIGINT trapped ..." << '\n';
-      _condition.notify_one();
-    }
-  }
-
-  static void waitForUserInterrupt() {
-    std::unique_lock<std::mutex> lock { _mutex };
-    _condition.wait(lock);
-    std::cout << "user has signaled to interrup program..." << '\n';
-    lock.unlock();
   }
 };
 
