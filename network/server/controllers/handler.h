6a6547f4e9af7d700423333fecb804836e85b70d
Roman <roma_klimovitskiy@mail.ru>
2020-04-07 00:29:50 +0300

    Added default response for handler (404, Not Found)

--- network/server/controllers/handler.h
+++ network/server/controllers/handler.h
@@ -4,20 +4,22 @@
 
 namespace network {
 
+using web::http::status_codes;
+
 class Handler {
  public:
   virtual ~Handler() = 0;
 
-  virtual void handleGet(web::http::http_request request) { }
-  virtual void handlePost(web::http::http_request request) { }
-  virtual void handlePut(web::http::http_request request) { }
-  virtual void handleHead(web::http::http_request request) { }
-  virtual void handleDelete(web::http::http_request request) { }
-  virtual void handleConnect(web::http::http_request request) { }
-  virtual void handleOptions(web::http::http_request request) { }
-  virtual void handleTrace(web::http::http_request request) { }
-  virtual void handlePatch(web::http::http_request request) { }
-  virtual void handleMerge(web::http::http_request request) { }
+  virtual void handleGet(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handlePost(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handlePut(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handleHead(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handleDelete(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handleConnect(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handleOptions(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handleTrace(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handlePatch(web::http::http_request request) { request.reply(status_codes::NotFound); }
+  virtual void handleMerge(web::http::http_request request) { request.reply(status_codes::NotFound); }
 };
 
 inline Handler::~Handler() { }
