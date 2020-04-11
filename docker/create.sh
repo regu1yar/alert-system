c0f23fd8b5fe7514e770042955c7e14fb250fd5e
Roman <roma_klimovitskiy@mail.ru>
2020-04-11 19:08:47 +0300

    Issue: II-61. Added docker container, scripts for running docker container

---
+++ docker/create.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+context=$( echo "$DIR/../")
+echo "Docker context: $context"
+
+docker build -t alert-system-build $context -f $DIR/Dockerfile
+docker rmi $(docker images -f "dangling=true" -q)
+
---
+++ docker/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker run --rm -p 42000:42000 --name alert-system alert-system-build
+
