aaadee34fd2c40b7163f0551a5ffbf856bedf625
Roman <roma_klimovitskiy@mail.ru>
2020-05-16 13:36:50 +0300

    Issue: II-198. Removed unnecessary target, added "protobufed" Alert class.

--- kapacitor_input/alert_handler.cpp
+++ kapacitor_input/alert_handler.cpp
@@ -8,49 +8,48 @@
 #include <cpprest/json.h>
 #include <cpprest/uri.h>
 #include <matrix_alerter/matrix_factory.h>
-#include <alert.h>
+//#include <alert.h>
+#include "alert.pb.h"
 #include <telegram_alerter/telegram_factory.h>
 #include "abstract_sender.h"
 
-void AlertHandler::handlePost(network::http_request message){
-    auto alert_body_task = message.extract_json();
+void AlertHandler::handlePost(network::http_request message) {
+  auto alert_body_task = message.extract_json();
 
-    std::cout << "New Request!" << std::endl;
+  std::cout << "New Request!" << std::endl;
 
-    alert_body_task.wait();
-    web::json::value alert_body = alert_body_task.get();
+  alert_body_task.wait();
+  web::json::value alert_body = alert_body_task.get();
 
-    std::stringstream formatted;
-    formatted << "Alert! Host/metric " << alert_body.at("id").as_string()
-                                 << " at " << alert_body.at("time").as_string() << std::endl;
-    try {
-        std::shared_ptr <Factory> factory;
-        Alert alert;
-        if(alert_body.has_field("matrix_id")) {
-            factory = std::shared_ptr<MatrixFactory>(new MatrixFactory());
-            alert.chat_id = alert_body.at("matrix_id").as_string();
-            std::cout << "Going to Matrix" << std::endl;
-        }
-        else if(alert_body.has_field("chat_id")) {
-            factory = std::shared_ptr<TelegramFactory>(new TelegramFactory());
-            alert.chat_id = alert_body.at("chat_id").as_string();
-            std::cout << "Going to Telegram" << std::endl;
-        }
-        else {
-            // ONLY FOR TESTING PURPOSES. TEST + KAPACITOR
-            factory = std::shared_ptr<MatrixFactory>(new MatrixFactory());
-            alert.chat_id = "!peaLsZdtyOLKogIHZJ:matrix.org";
-            std::cout << "Going to Matrix" << std::endl;
-        }
-        auto preparer = factory->createPreparer();
-        auto sender = factory->createSender();
-        alert.text = formatted.str();
-        auto prepared_alert = preparer->prepare(alert);
-        sender->send(prepared_alert);
-        std::cout << "SENT!" << std::endl;
-        message.reply(web::http::status_codes::OK);
-
-    } catch (std::exception) {
-        message.reply(web::http::status_codes::ServiceUnavailable);
+  std::stringstream formatted;
+  formatted << "Alert! Host/metric " << alert_body.at("id").as_string()
+            << " at " << alert_body.at("time").as_string() << std::endl;
+  try {
+    std::shared_ptr<Factory> factory;
+    Alert alert;
+    if (alert_body.has_field("matrix_id")) {
+      factory = std::shared_ptr<MatrixFactory>(new MatrixFactory());
+      alert.set_chat_id(alert_body.at("matrix_id").as_string());
+      std::cout << "Going to Matrix" << std::endl;
+    } else if (alert_body.has_field("chat_id")) {
+      factory = std::shared_ptr<TelegramFactory>(new TelegramFactory());
+      alert.set_chat_id(alert_body.at("chat_id").as_string());
+      std::cout << "Going to Telegram" << std::endl;
+    } else {
+      // ONLY FOR TESTING PURPOSES. TEST + KAPACITOR
+      factory = std::shared_ptr<MatrixFactory>(new MatrixFactory());
+      alert.set_chat_id("!peaLsZdtyOLKogIHZJ:matrix.org");
+      std::cout << "Going to Matrix" << std::endl;
     }
+    auto preparer = factory->createPreparer();
+    auto sender = factory->createSender();
+    alert.set_text(formatted.str());
+    auto prepared_alert = preparer->prepare(alert);
+    sender->send(prepared_alert);
+    std::cout << "SENT!" << std::endl;
+    message.reply(web::http::status_codes::OK);
+
+  } catch (std::exception) {
+    message.reply(web::http::status_codes::ServiceUnavailable);
+  }
 }
