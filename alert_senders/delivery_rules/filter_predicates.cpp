557b3115cb21e93246fa30db644a7c8b03409766
Roman <roma_klimovitskiy@mail.ru>
2020-05-18 15:21:17 +0300

    Issue: II-199. Implemented several filter predicates

--- alert_senders/delivery_rules/filter_predicates.cpp
+++ alert_senders/delivery_rules/filter_predicates.cpp
@@ -3,3 +3,250 @@
 FilterPredicate::~FilterPredicate() {
 
 }
+
+NegationFilterPredicate::NegationFilterPredicate(std::shared_ptr<FilterPredicate> predicate)
+    : operand_predicate_(predicate) {
+
+}
+
+NegationFilterPredicate::~NegationFilterPredicate() {
+
+}
+
+bool NegationFilterPredicate::isTrue(const alert::Alert &alert) const {
+  return !operand_predicate_->isTrue(alert);
+}
+
+AndFilterPredicate::AndFilterPredicate(std::shared_ptr<FilterPredicate> first_predicate,
+                                       std::shared_ptr<FilterPredicate> second_predicate)
+    : first_operand_predicate_(first_predicate)
+    , second_operand_predicate_(second_predicate) {
+
+}
+
+AndFilterPredicate::~AndFilterPredicate() {
+
+}
+
+bool AndFilterPredicate::isTrue(const alert::Alert &alert) const {
+  return first_operand_predicate_->isTrue(alert) && second_operand_predicate_->isTrue(alert);
+}
+
+OrFilterPredicate::OrFilterPredicate(std::shared_ptr<FilterPredicate> first_predicate,
+                                     std::shared_ptr<FilterPredicate> second_predicate)
+    : first_operand_predicate_(first_predicate)
+    , second_operand_predicate_(second_predicate) {
+
+}
+
+OrFilterPredicate::~OrFilterPredicate() {
+
+}
+
+bool OrFilterPredicate::isTrue(const alert::Alert &alert) const {
+  return first_operand_predicate_->isTrue(alert) || second_operand_predicate_->isTrue(alert);
+}
+
+EqualsTagPredicate::EqualsTagPredicate(const std::string &tag, const std::string &value)
+    : tag_(tag), value_(value) {
+
+}
+
+EqualsTagPredicate::~EqualsTagPredicate() {
+
+}
+
+bool EqualsTagPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    if (series.tags().contains(tag_) && series.tags().at(tag_) == value_) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+NotEqualsTagPredicate::NotEqualsTagPredicate(const std::string &tag, const std::string &value)
+    : tag_(tag), value_(value) {
+
+}
+
+NotEqualsTagPredicate::~NotEqualsTagPredicate() {
+
+}
+
+bool NotEqualsTagPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    if (series.tags().contains(tag_) && series.tags().at(tag_) != value_) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+HasTagPredicate::HasTagPredicate(const std::string &tag) : tag_(tag) {
+
+}
+
+HasTagPredicate::~HasTagPredicate() {
+
+}
+
+bool HasTagPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    if (series.tags().contains(tag_)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+EqualsMetricPredicate::EqualsMetricPredicate(const std::string &metric_name, double value)
+    : metric_name_(metric_name), value_(value) {
+
+}
+
+EqualsMetricPredicate::~EqualsMetricPredicate() {
+
+}
+
+bool EqualsMetricPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    for (const auto& metric : series.metrics()) {
+      if (metric.values().contains(metric_name_) && metric.values().at(metric_name_) == value_) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+LessMetricPredicate::LessMetricPredicate(const std::string &metric_name, double value)
+    : metric_name_(metric_name), threshold_value_(value) {
+
+}
+
+LessMetricPredicate::~LessMetricPredicate() {
+
+}
+
+bool LessMetricPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    for (const auto& metric : series.metrics()) {
+      if (metric.values().contains(metric_name_) && metric.values().at(metric_name_) < threshold_value_) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+GreaterMetricPredicate::GreaterMetricPredicate(const std::string &metric_name, double value)
+    : metric_name_(metric_name), threshold_value_(value) {
+
+
+}
+
+GreaterMetricPredicate::~GreaterMetricPredicate() {
+
+}
+
+bool GreaterMetricPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    for (const auto& metric : series.metrics()) {
+      if (metric.values().contains(metric_name_) && metric.values().at(metric_name_) > threshold_value_) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+NotEqualsMetricPredicate::NotEqualsMetricPredicate(const std::string &metric_name, double value)
+    : metric_name_(metric_name), value_(value) {
+
+}
+
+NotEqualsMetricPredicate::~NotEqualsMetricPredicate() {
+
+}
+
+bool NotEqualsMetricPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    for (const auto& metric : series.metrics()) {
+      if (metric.values().contains(metric_name_) && metric.values().at(metric_name_) != value_) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+LessOrEqualsMetricPredicate::LessOrEqualsMetricPredicate(const std::string &metric_name, double value)
+    : metric_name_(metric_name), threshold_value_(value) {
+
+}
+
+LessOrEqualsMetricPredicate::~LessOrEqualsMetricPredicate() {
+
+}
+
+bool LessOrEqualsMetricPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    for (const auto& metric : series.metrics()) {
+      if (metric.values().contains(metric_name_) && metric.values().at(metric_name_) <= threshold_value_) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+GreaterOrEqualsMetricPredicate::GreaterOrEqualsMetricPredicate(const std::string &metric_name, double value)
+    : metric_name_(metric_name), threshold_value_(value) {
+
+
+}
+
+GreaterOrEqualsMetricPredicate::~GreaterOrEqualsMetricPredicate() {
+
+}
+
+bool GreaterOrEqualsMetricPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    for (const auto& metric : series.metrics()) {
+      if (metric.values().contains(metric_name_) && metric.values().at(metric_name_) >= threshold_value_) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+HasMetricPredicate::HasMetricPredicate(const std::string &metric_name) : metric_name_(metric_name) {
+
+}
+
+HasMetricPredicate::~HasMetricPredicate() {
+
+}
+
+bool HasMetricPredicate::isTrue(const alert::Alert &alert) const {
+  for (const auto& series : alert.series()) {
+    for (const auto& metric : series.metrics()) {
+      if (metric.values().contains(metric_name_)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
--- alert_senders/delivery_rules/filter_predicates.h
+++ alert_senders/delivery_rules/filter_predicates.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 #include "alert.pb.h"
@@ -9,4 +10,185 @@ class FilterPredicate {
   virtual ~FilterPredicate() = 0;
 
   virtual bool isTrue(const alert::Alert& alert) const = 0;
+};
+
+
+class NegationFilterPredicate : public FilterPredicate {
+ public:
+  explicit NegationFilterPredicate(std::shared_ptr<FilterPredicate> predicate);
+
+  ~NegationFilterPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::shared_ptr<FilterPredicate> operand_predicate_;
+};
+
+
+class AndFilterPredicate : public FilterPredicate {
+ public:
+  AndFilterPredicate(std::shared_ptr<FilterPredicate> first_predicate,
+                     std::shared_ptr<FilterPredicate> second_predicate);
+
+  ~AndFilterPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::shared_ptr<FilterPredicate> first_operand_predicate_;
+  std::shared_ptr<FilterPredicate> second_operand_predicate_;
+};
+
+
+class OrFilterPredicate : public FilterPredicate {
+ public:
+  OrFilterPredicate(std::shared_ptr<FilterPredicate> first_predicate,
+  std::shared_ptr<FilterPredicate> second_predicate);
+
+  ~OrFilterPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::shared_ptr<FilterPredicate> first_operand_predicate_;
+  std::shared_ptr<FilterPredicate> second_operand_predicate_;
+};
+
+
+class EqualsTagPredicate : public FilterPredicate {
+ public:
+  EqualsTagPredicate(const std::string& tag, const std::string& value);
+
+  ~EqualsTagPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string tag_;
+  std::string value_;
+};
+
+
+class NotEqualsTagPredicate : public FilterPredicate {
+ public:
+  NotEqualsTagPredicate(const std::string& tag, const std::string& value);
+
+  ~NotEqualsTagPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string tag_;
+  std::string value_;
+};
+
+
+class HasTagPredicate : public FilterPredicate {
+ public:
+  explicit HasTagPredicate(const std::string& tag);
+
+  ~HasTagPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string tag_;
+};
+
+
+class EqualsMetricPredicate : public FilterPredicate {
+ public:
+  EqualsMetricPredicate(const std::string& metric_name, double value);
+
+  ~EqualsMetricPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string metric_name_;
+  double value_;
+};
+
+
+class NotEqualsMetricPredicate : public FilterPredicate {
+ public:
+  NotEqualsMetricPredicate(const std::string& metric_name, double value);
+
+  ~NotEqualsMetricPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string metric_name_;
+  double value_;
+};
+
+
+class LessMetricPredicate : public FilterPredicate {
+ public:
+  LessMetricPredicate(const std::string& metric_name, double value);
+
+  ~LessMetricPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string metric_name_;
+  double threshold_value_;
+};
+
+
+class GreaterMetricPredicate : public FilterPredicate {
+ public:
+  GreaterMetricPredicate(const std::string& metric_name, double value);
+
+  ~GreaterMetricPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string metric_name_;
+  double threshold_value_;
+};
+
+
+class LessOrEqualsMetricPredicate : public FilterPredicate {
+ public:
+  LessOrEqualsMetricPredicate(const std::string& metric_name, double value);
+
+  ~LessOrEqualsMetricPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string metric_name_;
+  double threshold_value_;
+};
+
+
+class GreaterOrEqualsMetricPredicate : public FilterPredicate {
+ public:
+  GreaterOrEqualsMetricPredicate(const std::string& metric_name, double value);
+
+  ~GreaterOrEqualsMetricPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string metric_name_;
+  double threshold_value_;
+};
+
+
+class HasMetricPredicate : public FilterPredicate {
+ public:
+  explicit HasMetricPredicate(const std::string& metric_name);
+
+  ~HasMetricPredicate() override;
+
+  bool isTrue(const alert::Alert& alert) const override;
+
+ private:
+  std::string metric_name_;
 };
